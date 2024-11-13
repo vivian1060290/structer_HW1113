967784c1e7e334d1063937178a6b294f1c161969
范芷紜 <107301464+vivian1060290@users.noreply.github.com>
2024-11-13 19:27:09 +0800

    Update Source.cpp

--- githubHW_1113/bin/Source.cpp
+++ githubHW_1113/bin/Source.cpp
@@ -1,76 +1,54 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
 using namespace std;
 
-class DataHandler {
-private:
-    char type;  // 資料類型 'i' 或 's'
-    int intD[30]; // 整數資料，最多 30 筆
-    string strD[30]; // 字串資料，最多 30 筆
-    int n; // 資料的筆數
+template <typename T>
+void binarySearch(int n, int q) {
+    T query;
+    T* data = new T[n];
 
-public:
-    // 建構子初始化資料類型並讀取資料
-    DataHandler(char t, int n) : type(t), n(n) {
-        if (type == 'i') {
-            for (int i = 0; i < n; i++) {
-                cin >> intD[i];
-            }
-        } else if (type == 's') {
-            for (int i = 0; i < n; i++) {
-                cin >> strD[i];
-            }
-        }
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> data[i];
     }
 
-    // 查詢並輸出目標的索引
-    void q(int q) {
-        if (type == 'i') {
-            for (int j = 0; j < q; j++) {
-                int t;
-                cin >> t;
-                int index = fInt(t);
-                cout << index << endl;
+  
+    for (int i = 0; i < q; i++) {
+        cin >> query;
+        int left = 0, right = n - 1;
+        bool found = false;
+
+      
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (data[mid] < query) {
+                left = mid + 1;
             }
-        } else if (type == 's') {
-            for (int j = 0; j < q; j++) {
-                string t;
-                cin >> t;
-                int index = fStr(t);
-                cout << index << endl;
+            else if (data[mid] > query) {
+                right = mid - 1;
+            }
+            else {
+                cout << mid << '\n';  
+                found = true;
+                break;
             }
         }
+        if (!found) cout << -1 << '\n';  
     }
 
-private:
-    // 查找整數目標的索引，找不到則返回 -1
-    int fInt(int t) {
-        for (int i = 0; i < n; i++) {
-            if (intD[i] == t) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    // 查找字串目標的索引，找不到則返回 -1
-    int fStr(const string& t) {
-        for (int i = 0; i < n; i++) {
-            if (strD[i] == t) {
-                return i;
-            }
-        }
-        return -1;
-    }
-};
-
+    delete[] data; 
 int main() {
-    char T;  // 資料類型
-    int n, q;  // 資料筆數和查詢數量
-    cin >> T >> n >> q;
+    char type;
+    int n = 0, q = 0;
 
-    DataHandler handler(T, n);  // 建立資料處理器
-    handler.q(q);  // 執行查詢
+    while (cin >> type >> n >> q) {
+        if (type == 'i') {
+            binarySearch<int>(n, q);
+        }
+        if (type == 's') {
+            binarySearch<string>(n, q);
+        }
+    }
 
     return 0;
 }
